d98e043af395c695734a452c4184b3a248094354
srishti1837 <srishtigupta1837@gmail.com>
2025-08-06 17:30:57 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from Vercel + FastAPI"}
+
+# This is needed for Vercel serverless to understand the app
+handler = Mangum(app)
